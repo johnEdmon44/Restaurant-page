059d5dfcde86c45b0880fedd83f999e18d1aedf6
Nessaj44 <johnedmoncoc@gmail.com>
2022-10-11 09:11:16 +0800

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -7,3 +7,25 @@ import './css/menu.css';
 import './css/contact.css';
 import './contact';
 'use strict';
+
+const homeBtn = document.querySelector('#nav-home');
+const menuBtn = document.querySelector('#nav-menu');
+const contactBtn = document.querySelector('#nav-contact');
+
+homeBtn.addEventListener('click', () => {
+  home.style = 'visibility: visible';
+  menu.style = 'visibility: hidden';
+  contact.style = 'visibility: hidden';
+});
+
+menuBtn.addEventListener('click', () => {
+  home.style = 'visibility: hidden';
+  menu.style = 'visibility: visible';
+  contact.style = 'visibility: hidden';
+});
+
+contactBtn.addEventListener('click', () => {
+  home.style = 'visibility: hidden';
+  menu.style = 'visibility: hidden';
+  contact.style = 'visibility: visible';
+});
